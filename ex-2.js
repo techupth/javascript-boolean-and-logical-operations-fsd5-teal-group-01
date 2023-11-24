44ac493f4577db3d5c775a044b5f7fb70db4a556
Krit <titatit.akkh@gmail.com>
2023-11-24 13:21:50 +0700

    feat: Ex1-3

--- ex-2.js
+++ ex-2.js
@@ -7,6 +7,14 @@ let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
 
+lastMonthPaidMoreThan4000 = 4001;
+isWeekday = "Friday";
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = false;
+isPlatinum = "Gold"
+
 let hasPromotion;
+hasPromotion = (lastMonthPaidMoreThan4000 > 4000) && (isWeekday = "Friday") && (!hasBoughtProductFromITCategory) 
+&& (!hasAttendedDiscountEvent) || (isPlatinum = "Gold");
 
 console.log(hasPromotion);
